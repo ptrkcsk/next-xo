46e0005d682637709e65f49ba1b45a481019124d
Patrik Csak <p@trikcsak.com>
2021-11-11 08:45:54 -0800

    Remove ESLint and use Next.js's ESLint plugin

--- pages/index.js
+++ pages/index.js
@@ -64,6 +64,12 @@ export default function Home() {
           </span>
         </a>
       </footer>
+
+      {/* Synchronous script to demonstrate that Next.js's ESLint rules are active */}
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossOrigin="anonymous"></script>
     </div>
   )
 }
